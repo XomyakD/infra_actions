d1a2acc5512898999ce497d81d73e0760d540f3b
XomyakD <114057285+XomyakD@users.noreply.github.com>
2023-05-25 15:59:21 +0500

    docker hub server run test 4

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!)хмхмхм')
+    return HttpResponse('У меня получилось!)хмхмхмм')
 
 
 def second_page(request):
